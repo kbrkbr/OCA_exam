9f8d61c3b7691d7410755f02198f190a8bfb0d41
Philipp Kober <philipp.kober@wirecube.at>
2016-05-13 12:28:14 +0200

    Begin chapter 4

---
+++ chapter4/OcaMethodsAndEncapsulation.java
@@ -0,0 +1,26 @@
+package chapter4;
+
+public class OcaMethodsAndEncapsulation {
+
+    public static void main(String[] args) {
+
+	// access specifiers
+	// public - any
+	// private - only class itself
+	// protected - only class and descendants and classes in same package
+	
+	// method names
+	// letters, numbers, $, _ first letter cannot be a number
+	
+	// exceptions
+	// optional, multiple exceptions seperated by comma
+	
+	
+	
+	
+	
+	
+	
+    }
+
+}
