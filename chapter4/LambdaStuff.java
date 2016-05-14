238ad026a88dc7badd6862b39c5bd0f8f9c1b272
Philipp Kober <philipp.kober@wirecube.at>
2016-05-14 13:25:46 +0200

    added lambda stuff

---
+++ chapter4/LambdaStuff.java
@@ -0,0 +1,74 @@
+package chapter4;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class LambdaStuff {
+
+    public static void main(String[] args) {
+	
+	List<Animal> animals = new ArrayList<Animal>();
+	
+	animals.add(new Animal("fish", false, true));
+	animals.add(new Animal("kangaroo", true, false));
+	animals.add(new Animal("dog", true, true));
+	animals.add(new Animal("karpador", false, false));
+	
+	print(animals, new CheckIfHopper());
+	print(animals, a -> a.canHop());
+	
+    }
+    
+    public static void print(List<Animal> animals, CheckTrait hopChecker) {
+	
+	for(Animal animal : animals) {
+	    System.out.println(String.format("%-8s %s %s", 
+		    animal, " can hop?", hopChecker.test(animal)));
+	}
+	
+    }
+
+}
+
+
+class Animal {
+
+    private String species;
+    private boolean canHop;
+    private boolean canSwim;
+    
+    public Animal(String speciesName, boolean hopper, boolean swimmer) {
+	this.species = speciesName;
+	this.canHop = hopper;
+	this.canSwim = swimmer;	
+    }
+    
+    public boolean canHop() {
+	return this.canHop;
+    }
+    
+    public boolean canSwim() {
+	return this.canSwim;
+    }
+    
+    public String toString() {
+	return this.species;
+    }
+        
+}
+
+
+interface CheckTrait {
+    
+    public boolean test(Animal a);
+    
+}
+
+
+class CheckIfHopper implements CheckTrait {
+
+    public boolean test(Animal a) {
+	return a.canHop();
+    }   
+    
+}
--- chapter4/OcaMethodsAndEncapsulation.java
+++ chapter4/OcaMethodsAndEncapsulation.java
@@ -3,7 +3,9 @@ package chapter4;
 public class OcaMethodsAndEncapsulation {
 
     public static void main(String[] args) {
-
+	
+	System.out.println("Hio there");
+	
 	// access specifiers
 	// public - any
 	// private - only class itself
@@ -15,12 +17,46 @@ public class OcaMethodsAndEncapsulation {
 	// exceptions
 	// optional, multiple exceptions seperated by comma
 	
+	// static imports
+	// import static java.util.Arrays.asList;
+	// import static java.util.Arrays.*;
 	
+	// all parameters passed by reference
 	
+	// overloading
+	// at least change parameter list
+	// varargs == array
+	// order: exact match, wider types, autoboxed type, varargs
+	// conversion: only in one step, putting int into Long wont work
 	
+	//constructors
+	// call different constructor in constructor: this(bla, blub);
 	
+	// !
+	// oder of initialization
+	// 1. superclass initialization
+	// 2. static var init and static initializers
+	// 3. instance var decl and instance initializers
+	// 4. Constructor
+	// !
 	
+	// immutable classes
+	// get and set a copy to get true immutability
+	
+	// lambdas
+	//
 	
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
