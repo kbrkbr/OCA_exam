7963ea635e88b3726df706e592da2a2e2030b4fc
Philipp Kober <philipp.kober@wirecube.at>
2016-05-19 16:46:52 +0200

    finished oca prep files

---
+++ src/chapter4/LambdaPlusPlus.java
@@ -0,0 +1,19 @@
+package chapter4;
+
+interface Climb {
+    boolean isTooHigh(int height, int limit);
+
+}
+
+class Climber {
+    public static void main(String[] args) {
+
+        try {
+            System.exit(2);
+        } finally {
+            System.out.print("bla");
+        }
+
+
+    }
+    }
---
+++ src/chapter6/Exceptions.java
@@ -0,0 +1,9 @@
+package chapter6;
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+    }
+
+
+}
